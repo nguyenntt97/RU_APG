3dd91884d2e59af1faca3b6cd0fa423c36007286
nguyenntt3 <nguyenntt3@vng.com.vn>
2021-10-19 10:05:01 +0700

    add ignore list

---
+++ apg_project/group2_apg/web_client/bot.js
@@ -0,0 +1,51 @@
+const tmi = require('tmi.js');
+
+// secret gqvz6nhzvtj646oz2knr6scmt6uj6g
+// Define configuration options
+const opts = {
+  identity: {
+    username: "nguyenntt",
+    password: "oauth:60jeclq4fu9031b4ffxidnd2r5jxgp"
+  },
+  channels: [
+    "nguyenntt"
+  ]
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  // If the command is known, let's execute it
+  if (commandName === '!dice') {
+    const num = rollDice();
+    client.say(target, `You rolled a ${num}`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+// Function called when the "dice" command is issued
+function rollDice () {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
