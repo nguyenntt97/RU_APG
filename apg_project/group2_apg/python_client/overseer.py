64c3c7500e54da095d401800eb19a7ed5341a29f
elchris <nguyenng178@gmail.com>
2021-11-13 22:48:58 +0700

    add overseer for capturing game state

--- apg_project/group2_apg/python_client/overseer.py
+++ apg_project/group2_apg/python_client/overseer.py
@@ -3,8 +3,11 @@ import win32ui
 from ctypes import windll
 from PIL import Image
 
-def main():
-    hwnd = win32gui.FindWindow(0, "Pacman")
+import numpy as np
+import cv2
+
+
+def capture(hwnd):
     left, top, right, bot = win32gui.GetClientRect(hwnd)
     w = right - left
     h = bot - top
@@ -18,26 +21,36 @@ def main():
 
     saveDC.SelectObject(saveBitMap)
     result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 1)
-    print(result)
 
-    bmpinfo = saveBitMap.GetInfo()
-    bmpstr = saveBitMap.GetBitmapBits(True)
+    if (result == 1):
+        bmpinfo = saveBitMap.GetInfo()
+        bmpstr = saveBitMap.GetBitmapBits(True)
 
-    im = Image.frombuffer(
-        'RGB',
-        (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
-        bmpstr, 'raw', 'BGRX', 0, 1)
+        im = Image.frombuffer(
+            'RGB',
+            (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+            bmpstr, 'raw', 'BGRX', 0, 1)
 
-    win32gui.DeleteObject(saveBitMap.GetHandle())
-    saveDC.DeleteDC()
-    mfcDC.DeleteDC()
-    win32gui.ReleaseDC(hwnd, hwndDC)
+        win32gui.DeleteObject(saveBitMap.GetHandle())
+        saveDC.DeleteDC()
+        mfcDC.DeleteDC()
+        win32gui.ReleaseDC(hwnd, hwndDC)
 
-    if result == 1:
-        # PrintWindow Succeeded
-        im.save("test.png")
+        return im
 
-    return 1
+    return None
+
+
+def main():
+    hwnd = win32gui.FindWindow(0, "Pacman")
+
+    while True:
+        img = capture(hwnd)
+        cv2.imshow('screen', np.array(img))
+
+        if (cv2.waitKey(1) & 0xFF) == ord('q'):
+            cv2.destroyAllWindows()
+            break
 
 
 if __name__ == '__main__':
